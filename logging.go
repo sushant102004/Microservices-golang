719983780d8b3340b63c38a8b2df5879476da886
Sushant <sushant.dhiman9812@gmail.com>
2023-10-08 10:03:55 +0530

    Created Logging Service

---
+++ logging.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+type LoggingService struct {
+	next PriceFetcher
+}
+
+func NewLoggingService(next PriceFetcher) PriceFetcher {
+	return &LoggingService{
+		next: next,
+	}
+}
+
+func (l *LoggingService) FetchPrice(ctx context.Context, crypto string) (price float64, err error) {
+	defer func(sTime time.Time) {
+		logrus.WithFields(logrus.Fields{
+			"took":  time.Since(sTime),
+			"error": err,
+			"price": price,
+		}).Info("Fetch Price")
+	}(time.Now())
+
+	return l.next.FetchPrice(ctx, crypto)
+}
---
+++ service.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type PriceFetcher interface {
+	FetchPrice(context.Context, string) (float64, error)
+}
+
+type priceFetcher struct{}
+
+var priceData = map[string]float64{
+	"BTC": 32000,
+	"ETH": 200,
+}
+
+func (s *priceFetcher) FetchPrice(ctx context.Context, crypto string) (float64, error) {
+	return CryptoPriceFetcher(ctx, crypto)
+}
+
+func CryptoPriceFetcher(ctx context.Context, crypto string) (float64, error) {
+	price, ok := priceData[crypto]
+	if !ok {
+		return price, fmt.Errorf("price not found for %s", crypto)
+	}
+
+	return price, nil
+}
